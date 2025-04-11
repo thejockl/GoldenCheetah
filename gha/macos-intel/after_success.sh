92b4abf8b586b64c4f2cc8525f9d0d0802f98517
Joachim Kohlhammer <joachim.kohlhammer@gmx.de>
2025-04-11 22:37:31 +0200

    reduced use of FINAL_NAME

--- gha/macos-intel/after_success.sh
+++ gha/macos-intel/after_success.sh
@@ -13,18 +13,16 @@ export DYLD_FALLBACK_LIBRARY_PATH=/opt/homebrew/lib:$DYLD_FALLBACK_LIBRARY_PATH
 # Initial deployment using macdeployqt
 macdeployqt GoldenCheetah.app -verbose=2 -fs=hfs+ -dmg -executable=GoldenCheetah.app/Contents/MacOS/GoldenCheetah
 
-tree GoldenCheetah.app
-ls
-
 echo "Renaming dmg file to branch and build number ready for deploy"
 # export FINAL_NAME=GoldenCheetah_v3.7_x64-gha.dmg
-mv GoldenCheetah.dmg $FINAL_NAME
-ls -l $FINAL_NAME
+# mv GoldenCheetah.dmg $FINAL_NAME
 
 # Add last commit message and SHA256 to txt file
 git log -1 >> GCversionMacOS.txt
 echo "SHA256 hash of $FINAL_NAME:" >> GCversionMacOS.txt
-shasum -a 256 $FINAL_NAME | cut -f 1 -d ' ' >> GCversionMacOS.txt
+shasum -a 256 GoldenCheetah.dmg | cut -f 1 -d ' ' >> GCversionMacOS.txt
 cat GCversionMacOS.txt
+
+# ls -l $FINAL_NAME
 
 exit
